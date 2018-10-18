20abe520ff402446ddc21afe997b07a05d3442b4
Martin Clausen <martin.clausen@gmail.com>
2018-10-18 21:57:59 +0200

    Update app.cljs

--- src/cljs/oz/app.cljs
+++ src/cljs/oz/app.cljs
@@ -1,1 +1,68 @@
+(ns ^:figwheel-always oz.core
+  (:require [reagent.core :as r]
+            [taoensso.encore :as encore :refer-macros (have)]
+            [taoensso.timbre :as timbre :refer-macros (debugf)]
+            [taoensso.sente :as sente]
+            [taoensso.sente.packers.transit :as sente-transit]))
 
+(timbre/set-level! :info)
+(enable-console-print!)
+
+(defn log [a-thing]
+  (.log js/console a-thing))
+
+(defonce app-state (r/atom {:text "Pay no attention to the man behind the curtain!"
+                            :view-spec nil}))
+
+(let [packer (sente-transit/get-transit-packer)
+      {:keys [chsk ch-recv send-fn state]}
+      (sente/make-channel-socket-client! "/chsk"
+                                         {:type :auto
+                                          :packer packer})]
+  (def chsk chsk)
+  (def ch-chsk ch-recv)
+  (def chsk-send! send-fn)
+  (def chsk-state state))
+
+(defmulti -event-msg-handler :id)
+
+(defn event-msg-handler [{:as ev-msg :keys [id ?data event]}]
+  (debugf "Event: %s" event) []
+  (-event-msg-handler ev-msg))
+
+(defmethod -event-msg-handler :default
+  [{:as ev-msg :keys [event]}]
+  (debugf "Unhandled event: %s" event))
+
+(defmethod -event-msg-handler :chsk/state
+  [{:as ev-msg :keys [?data]}]
+  (let [[old-state-map new-state-map] (have vector? ?data)]
+    (if (:first-open? new-state-map)
+      (debugf "Channel socket successfully established!: %s" ?data)
+      (debugf "Channel socket state change: %s" ?data))))
+
+(defmethod -event-msg-handler :chsk/handshake
+  [{:as ev-msg :keys [?data]}]
+  (let [[?uid ?csrf-token ?handshake-data] ?data]
+    (debugf "Handshake: %s" ?data)))
+
+;; This is the main event handler; If we want to do cool things with other kinds of data going back and forth,
+;; this is where we'll inject it.
+(defmethod -event-msg-handler :chsk/recv
+  [{:as ev-msg :keys [?data]}]
+  (let [[id msg] ?data]
+    (case id
+      :oz.core/view-spec (swap! app-state assoc :view-spec msg)
+      (debugf "Push event from server: %s" ?data))))
+
+(def router_ (atom nil))
+
+(defn stop-router! []
+  (when-let [stop-f @router_] (stop-f)))
+
+(defn start-router! []
+  (stop-router!)
+  (reset! router_ (sente/start-client-chsk-router! ch-chsk event-msg-handler)))
+
+(defn start! []
+  (start-router!))
